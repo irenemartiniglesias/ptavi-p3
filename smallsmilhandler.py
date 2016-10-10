987fcf8043e2a6c88aa217245933effecabd5685
irenemartiniglesias <i.martini@alumnos.urjc.es>
2016-10-10 12:33:23 +0200

    final pasado PEP8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -14,7 +14,7 @@ class SmallSMILHandler(ContentHandler):
 		Creamos una lista para almanenar nuestros atributos y sus valores
 		"""
 		self.lista = []
-		self.dic = {'root-layout' : ['width', 'height', 'background-color'], 
+		self.diccionario = {'root-layout' : ['width', 'height', 'background-color'], 
 		'region' : ['id', 'top', 'bottom', 'left', 'right'], 'img' : ['src',
 		'region', 'begin', 'dur'], 'audio' : ['src', 'begin', 'dur'], 
 		'textstream' : ['src', 'region']}
@@ -23,12 +23,12 @@ class SmallSMILHandler(ContentHandler):
 		"""
 		Método para abrir etiqueta y almacenar su contenido
 		"""
-		if name in self.dic:
-			dic_atributos = {}
-			for valor in self.dic[name]:
-				dic_atributos[valor] = atrrs.get(valor, "")
-			dic_etiquetas= {name : dic_atributos}
-			self.lista.append(dic_etiquetas)
+		if name in self.diccionario:
+			diccionario_atributos = {}
+			for valor in self.diccionario[name]:
+				diccionario_atributos[valor] = atrrs.get(valor, "")
+			diccionario_etiquetas= {name : diccionario_atributos}
+			self.lista.append(diccionario_etiquetas)
 
 	def get_tags(self):
 		return self.lista
